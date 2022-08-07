0d41fd3d1de5ac35cea98d41262f7f778efe28d1
blindstudio <blindstudio1@gmail.com>
2022-08-07 03:50:36 +0300

    ex10

---
+++ ex010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] Array = { 12, 23, 32, 44, 512, 63, 73, 44 };
+
+int n = Array.Length;
+int find = 44;
+
+int index = 0;
+
+while (index < n)
+{
+    if (Array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
