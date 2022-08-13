727fe03d191b2f6e42626cc75be68c334f4ded28
blindstudio <blindstudio1@gmail.com>
2022-08-13 11:22:44 +0300

    на уроке

---
+++ sem2/Program.cs
@@ -0,0 +1,17 @@
+﻿int randomNumber = new Random().Next(10, 100);
+
+int FirstDigit = randomNumber / 10;
+int SecondGigit = randomNumber % 10;
+
+if(FirstDigit > SecondGigit)
+{
+    Console.WriteLine(FirstDigit);
+}
+else
+{
+    Console.WriteLine(SecondGigit);
+}
+int GetRandomNumber(int min, int max)
+{
+    return new Random();
+}
