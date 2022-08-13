56c2178f3a38f91b8fe26a770bb2268bf3204429
blindstudio <blindstudio1@gmail.com>
2022-08-13 11:23:26 +0300

    закомментировал

--- sem2/Program.cs
+++ sem2/Program.cs
@@ -1,17 +1,17 @@
-﻿int randomNumber = new Random().Next(10, 100);
+﻿// int randomNumber = new Random().Next(10, 100);
 
-int FirstDigit = randomNumber / 10;
-int SecondGigit = randomNumber % 10;
+// int FirstDigit = randomNumber / 10;
+// int SecondGigit = randomNumber % 10;
 
-if(FirstDigit > SecondGigit)
-{
-    Console.WriteLine(FirstDigit);
-}
-else
-{
-    Console.WriteLine(SecondGigit);
-}
-int GetRandomNumber(int min, int max)
-{
-    return new Random();
-}
+// if(FirstDigit > SecondGigit)
+// {
+//     Console.WriteLine(FirstDigit);
+// }
+// else
+// {
+//     Console.WriteLine(SecondGigit);
+// }
+// int GetRandomNumber(int min, int max)
+// {
+//     return new Random();
+// }
